bd20091045d06938f783f608961526781663da1e
Vyacheslav Koscheev <vok1980@gmail.com>
2016-09-29 19:38:56 +0700

    Add method to check if gms available
    Change-Id: Ie200d46f33c372d69cc5d4fce1e5486112813dcc

--- src/plugins/position/android_gms/jar/src/org/qtproject/qt5/android/positioning/gms/QtPositioningGms.java
+++ src/plugins/position/android_gms/jar/src/org/qtproject/qt5/android/positioning/gms/QtPositioningGms.java
@@ -1,11 +1,24 @@
 
 package org.qtproject.qt5.android.positioning.gms;
 
+import android.app.Activity;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class QtPositioningGms implements ConnectionCallbacks, OnConnectionFailedListener
 {
     private static final String TAG = "QtPositioningGms";
 
+    static public boolean isAvailable(final Activity activity) {
+        try {
+            final GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+            final int errorCode = apiAvailability.isGooglePlayServicesAvailable(activity);
+            return ConnectionResult.SUCCESS == errorCode;
+        } catch(Exception e) {
+            Log.e(TAG, "Failed to check google api availability: ", e);
+        }
+
+        return false;
+    }
 
 }
 
