8f10d9ac817c5e780e890d9a7c723a938a0e77da
Vyacheslav Koscheev <vok1980@gmail.com>
2016-09-29 19:36:43 +0700

    Create class QtPositioningGms
    Change-Id: I7501fc2652273f9c45bfaccb2a9ff947765bbf71

---
+++ src/plugins/position/android_gms/jar/src/org/qtproject/qt5/android/positioning/gms/QtPositioningGms.java
@@ -0,0 +1,11 @@
+
+package org.qtproject.qt5.android.positioning.gms;
+
+
+public class QtPositioningGms implements ConnectionCallbacks, OnConnectionFailedListener
+{
+    private static final String TAG = "QtPositioningGms";
+
+
+}
+
